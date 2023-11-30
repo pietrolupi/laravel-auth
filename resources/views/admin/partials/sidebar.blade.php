5e74044cf502f98b66774dbfe77ccc64493ecd2a
pietrolupi <pietro.lupi92@gmail.com>
2023-11-30 14:30:34 +0100

    [add]: classe active nell'aside

--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -1,10 +1,10 @@
 <aside class="bg-dark">
     <nav>
         <ul>
-            <li><a href="{{route('admin.home')}}">Dashboard</a></li>
-            <li><a href="{{route('admin.projects.index')}}">Elenco Progetti</a></li>
-            <li><a href="{{route('admin.technologies.index')}}">Elenco Tecnologie</a></li>
-            <li><a href="{{route('admin.types.index')}}">Elenco tipologie</a></li>
+            <li><a class="{{Route::currentRouteName() === 'admin.home' ? 'active' : ''}}" href="{{route('admin.home')}}">Dashboard</a></li>
+            <li><a class="{{Route::currentRouteName() === 'admin.projects.index' ? 'active' : ''}}" href="{{route('admin.projects.index')}}">Elenco Progetti</a></li>
+            <li><a class="{{Route::currentRouteName() === 'admin.technologies.index' ? 'active' : ''}}" href="{{route('admin.technologies.index')}}">Elenco Tecnologie</a></li>
+            <li><a class="{{Route::currentRouteName() === 'admin.types.index' ? 'active' : ''}}" href="{{route('admin.types.index')}}">Elenco tipologie</a></li>
         </ul>
 
     </nav>
